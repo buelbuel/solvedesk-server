40db7767cfe2c31c252e94be619402ed624900ef
Alicem Bülbül <alicem.buelbuel@icloud.com>
2024-06-30 05:33:34 +0000

    Copy before Vite

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+/** PM2 Configuration */
+export const apps = [
+	{
+		name: 'solvedesk-server',
+		script: 'dist/index.js',
+		instances: 'max',
+		exec_mode: 'cluster',
+		env: {
+			NODE_ENV: 'production',
+			PORT: 3000
+		}
+	}
+]
